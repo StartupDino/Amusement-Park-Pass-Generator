c352cfaa36e860fc26ddee81c276e7792e07eafc
StartupDino <pnm326@gmail.com>
2017-06-30 15:33:21 -0400

    Added employee object file

---
+++ Amusement Park Pass Generator/Employee.swift
@@ -0,0 +1,21 @@
+//
+//  Employee.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Pete McPherson on 6/30/17.
+//  Copyright © 2017 Pete McPherson. All rights reserved.
+//
+
+import Foundation
+
+protocol Employee {
+    var firstName: String { get set }
+    var lastName: String { get set }
+    var address: String { get set }
+    var city: String { get set }
+    var state: String { get set }
+    var zipCode: Int { get set }
+    
+    var foodDiscount: Int { get set }
+    var merchDiscount: Int { get set }
+}
