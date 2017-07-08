efa30da0786c3403cedefe9af210cc0d3a41cb83
StartupDino <pnm326@gmail.com>
2017-07-08 08:09:45 -0400

    created Access protocol

---
+++ Amusement Park Pass Generator/Swiper.swift
@@ -0,0 +1,9 @@
+//
+//  Swiper.swift
+//  Amusement Park Pass Generator
+//
+//  Created by Pete McPherson on 7/7/17.
+//  Copyright © 2017 Pete McPherson. All rights reserved.
+//
+
+import Foundation
